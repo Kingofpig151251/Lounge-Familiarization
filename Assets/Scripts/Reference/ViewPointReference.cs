77cc54be1e45941351cc93a77e98ecbcfd550197
King <98532667+Kingofpig151251@users.noreply.github.com>
2024-02-01 20:56:01 +0800

    Update ViewPointReference.cs

--- Assets/Scripts/Reference/ViewPointReference.cs
+++ Assets/Scripts/Reference/ViewPointReference.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewPointReference : Singleton<ViewPointReference>
-{
-    public List<int> m_loungeStartIndex;
+{   
+    public List<int> m_loungeStartIndex; // 0 = Deck, 1 = Wing. 2 = Pier
     public List<ViewPoint> m_viewPointSO;
-
 }
