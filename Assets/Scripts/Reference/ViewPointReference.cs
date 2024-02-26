6e9f0ed5feb8be08fa3cf2422e57aa4092740137
King <98532667+Kingofpig151251@users.noreply.github.com>
2024-02-26 20:43:02 +0800

    adding Wing VP

--- Assets/Scripts/Reference/ViewPointReference.cs
+++ Assets/Scripts/Reference/ViewPointReference.cs
@@ -4,7 +4,7 @@ namespace Reference
 {
     public class ViewPointReference : Singleton<ViewPointReference>
     {   
-        public List<int> m_loungeStartIndex; // 0 = Deck, 1 = Wing. 2 = Pier
+        public List<int> m_loungeStartIndex; //     DeckBusinessLounge,WingFristClassLounge,WingBusinessLounge,PierFirstClassLounge,PierBusinessLounge
         public List<ViewPoint> m_viewPointSO;
     }
 }
