5b01ec569163b6499ed874694e9201bd187130ae
71629 <105049686+71629@users.noreply.github.com>
2024-05-16 17:29:26 +0800

    Track task complete history and allow resetting

--- Assets/Scripts/ScriptableObject/TaskSO.cs
+++ Assets/Scripts/ScriptableObject/TaskSO.cs
@@ -5,10 +5,30 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "New TaskConfigSO", menuName = "New TaskConfigSO", order = 4)]
 public class TaskSO : ScriptableObject
 {
-    public ulong m_taskID;
+    private const string TaskMask = "task-mask";
+    public static int Mask => PlayerPrefs.GetInt(TaskMask);
+    
+    public int m_taskID;
     [Space][FormerlySerializedAs("m_Regionid")] public Lounge m_lounge;
     [Space] public string m_question_TC;
     public string m_question_SC;
     public string m_question_ENG;
     public List<int> m_navigateIndex;
+    
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    public static void Initialize()
+    {
+        if (PlayerPrefs.HasKey(TaskMask)) return;
+        PlayerPrefs.SetInt(TaskMask, 0);
+    }
+    
+    public void CompleteTask()
+    {
+        PlayerPrefs.SetInt(TaskMask, Mask | m_taskID);
+    }
+    
+    public bool IsCompleted()
+    {
+        return (Mask & m_taskID) != 0;
+    }
 }
