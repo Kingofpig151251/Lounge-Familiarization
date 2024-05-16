4dcd1ad4612acf16855f56cb80133d47a261a188
71629 <105049686+71629@users.noreply.github.com>
2024-05-16 14:13:14 +0800

    Assign ID to tasks and questions

--- Assets/Scripts/ScriptableObject/TaskSO.cs
+++ Assets/Scripts/ScriptableObject/TaskSO.cs
@@ -5,7 +5,8 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "New TaskConfigSO", menuName = "New TaskConfigSO", order = 4)]
 public class TaskSO : ScriptableObject
 {
-    [FormerlySerializedAs("m_Regionid")] public Lounge m_lounge;
+    public ulong m_taskID;
+    [Space][FormerlySerializedAs("m_Regionid")] public Lounge m_lounge;
     [Space] public string m_question_TC;
     public string m_question_SC;
     public string m_question_ENG;
